9f457819ca82086cf38987930dc83d1956fbe66b
jejevj <wijaya.angga.j@gmail.com>
2025-05-18 21:35:05 +0700

    first commit

---
+++ assets/InviteFriends-58ba2f02.js
@@ -0,0 +1,3 @@
+import{K as r}from"./Card-9ebddc4f.js";import{K as i}from"./InviteFriendsModal-4e542f19.js";import{a as s}from"./assets-8bbe0335.js";import{d,_ as l,r as e,e as m,m as n,F as p,o as c}from"./index-e7dd6bfa.js";const _=d({name:"invite-friends",components:{KTModalCard:r,KTInviteFriendsModal:i},setup(){return{getIllustrationsPath:s}}});function f(t,u,v,F,I,K){const o=e("KTModalCard"),a=e("KTInviteFriendsModal");return c(),m(p,null,[n(o,{title:"Invite Friends Modal Example",description:`Click on the below buttons\r
+  to launch <br />\r
+  a friend invitation example.`,image:t.getIllustrationsPath("1.png"),"button-text":"Invite Friends","modal-id":"kt_modal_invite_friends"},null,8,["image"]),n(a)],64)}const h=l(_,[["render",f]]);export{h as default};
---
+++ assets/modal-441f49c3.js
@@ -0,0 +1,1 @@
+import{aw as n}from"./index-e7dd6bfa.js";const r=e=>{if(!e)return;const t=n.getInstance(e);t==null||t.hide()};export{r as h};
